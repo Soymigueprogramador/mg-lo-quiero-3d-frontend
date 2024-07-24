c9c27656931407be9e7794c8e273771a16014c8d
Miguel Guimar Salazar <116126244+Soymigueprogramador@users.noreply.github.com>
2024-07-23 22:47:35 -0300

    Pude solucionar parcialmente el problema del bug y ahora el cogido volvio a funcionar. tengo que ver porque no se aplican ciertos estilos a la card pero lo dejo para mañana

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -4,7 +4,7 @@ import Card from 'react-bootstrap/Card';
 import { useState, useEffect } from "react";
 import { getProductos } from '../../asyncMock.js';
 
-function BasicExample({ image, nombre, id, precio, stock }) {
+function BasicExample() {
   const [productos, setProductos] = useState([]);
 
   useEffect(() => {
@@ -14,17 +14,21 @@ function BasicExample({ image, nombre, id, precio, stock }) {
   }, []);
 
   return (
-    <Card className={styles.card} style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={image} alt={nombre} />
-      <Card.Body>
-        <h2 className={styles.text_card}>Nombre: {nombre}</h2>
-        <p className={styles.text_card}>ID: {id}</p>
-        <p className={styles.text_card}>Precio: {precio}</p>
-        <p className={styles.text_card}>Stock: {stock}</p>
-        <Button className={styles.btn_card}>Ver detalles</Button>
-        <Button className={styles.btn_card}>Comprar ahora</Button>
-      </Card.Body>
-    </Card>
+    <ul>
+      {productos.map((producto) => (
+        <Card key={producto.id} className={styles.card} style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={producto.image} alt={producto.nombre} />
+          <Card.Body>
+            <h2 className={styles.text_card}>Nombre: {producto.nombre}</h2>
+            <p className={styles.text_card}>ID: {producto.id}</p>
+            <p className={styles.text_card}>Precio: {producto.precio}</p>
+            <p className={styles.text_card}>Stock: {producto.stock}</p>
+            <Button className={styles.btn_card}>Ver detalles</Button>
+            <Button className={styles.btn_card}>Comprar ahora</Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </ul>
   );
 }
 
