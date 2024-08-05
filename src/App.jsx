97bf9b29fb59068a92ff76d2fdea1ef12e763e3e
Miguel Guimar Salazar <116126244+Soymigueprogramador@users.noreply.github.com>
2024-08-04 21:36:22 -0300

    Agregue la parte del context, cart, cartItem. ahora cada ves que hago click en ver detalle de un producto se abre una nueva seccion en la cual se ven detalles del producto. me quedan correjir esas estilos, hacer responsive las imagenes del inicio, terminar de acomodar las card de los productos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.scss";
 import {
   BrowserRouter as Router,
-  Route,
-  Routes,
+  Route, 
+  Routes
 } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar.jsx";
-//import ItemListContainer from "./components/ItemListContainer/ItemListContainer.jsx";
-//import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer.jsx";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer.jsx";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer.jsx";
+import Cart from './components/Cart/Cart.jsx';
 import Inicio from './page/Inicio/Inicio.jsx';
 import Productos from './page/Productos/Productos.jsx';
 import Contacto from './page/Contacto/Contacto.jsx';
@@ -21,6 +22,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/productos" element={<Productos />} />
+        <Route path="/categoria/:idCategoria" element={<ItemListContainer />} />
+        <Route path="/item/:idItem" element={<ItemDetailContainer />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/contacto" element={<Contacto />} />
         <Route path="*" element={<h1>El sitio {nameEcommerce} está en proceso de producción</h1>} />
       </Routes>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,30 @@
-
+import { useContext } from "react"
+import { CarritoContext } from '../../Context/CarritoContext.jsx'
+import { Link } from "react-router-dom"
+import CartItem from '../CartItem/CartItem.jsx'
 
 const Cart = () => {
+  const { carrito, vaciarCarrito, total, cantidadTotal } = useContext(CarritoContext);
+
+  if( cantidadTotal === 0 ) {
+    return (
+      <>
+        <h2> Carrito vacio </h2>
+        <Link to='/'> Ver productos </Link>
+      </>
+    )
+  }
+  
   return (
-    <div>Cart</div>
+    <div>
+      {
+        carrito.map( producto => <CartItem hey={producto.id} { ... producto } /> )
+      }
+      <h3> Total: ${ total } </h3>
+      <h3> Cantidad total: { cantidadTotal } </h3>
+      <button onClick={() => vaciarCarrito()}> Vaciar carrito </button>
+      <Link to='/checkout'> Finalizar compra </Link>
+    </div>
   )
 }
 
